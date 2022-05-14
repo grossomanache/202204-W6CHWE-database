a37f2deeb52656a870162ae25f21b314b5094a81
Elías Heresi <elias.heresi@gmail.com>
2022-05-14 21:17:24 +0200

    Setup msw handlers and servers

---
+++ src/mocks/handlers.js
@@ -0,0 +1,8 @@
+import { rest } from "msw";
+import { mockRobots } from "../redux/mocks/mockRobots";
+
+export const handlers = [
+  rest.get(process.env.REACT_APP_API_URL, (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(mockRobots))
+  ),
+];
