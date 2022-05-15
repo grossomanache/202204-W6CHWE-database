feea97905fe0513f263d235df68741a4715624df
Elías Heresi <elias.heresi@gmail.com>
2022-05-15 18:45:38 +0200

    Load detail page shows only when details have been retrieved

--- src/redux/thunks/thunks.test.js
+++ src/redux/thunks/thunks.test.js
@@ -1,4 +1,5 @@
 import { server } from "../../mocks/server";
+import { mockRobots } from "../mocks/mockRobots";
 import { loadRobotsThunk, loadRobotByIdThunk } from "./thunks";
 
 beforeEach(() => server.listen());
@@ -6,12 +7,25 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe("Given the loadRobotsThunk function", () => {
-  describe("When instantiated", () => {
-    test("Then dispatch will have been called", async () => {
-      const loadThunk = await loadRobotsThunk();
-      const dispatch = jest.fn();
-      await loadThunk(dispatch);
-      expect(dispatch).toHaveBeenCalled();
+  describe("When instantiated, having mockRobots in the database", () => {
+    test("Then dispatch will have been called, returning mockRobots", async () => {
+      const thunk = await loadRobotsThunk();
+      const dispatch = jest.fn().mockReturnValue(mockRobots);
+      await thunk(dispatch);
+      expect(dispatch).toHaveReturnedWith(mockRobots);
+    });
+  });
+});
+
+describe("Given the loadRobotById function", () => {
+  describe("When instantiated with the id 1", () => {
+    test("Then dispatch will be called, returning the Larry the boy robot", async () => {
+      const searchedId = 1;
+      const thunk = await loadRobotByIdThunk(searchedId);
+      const larryRobot = mockRobots[0];
+      const dispatch = jest.fn().mockReturnValue(larryRobot);
+      await thunk(dispatch);
+      expect(dispatch).toHaveReturnedWith(larryRobot);
     });
   });
 });
