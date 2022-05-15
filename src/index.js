37bfbe53745a4475eeb6d0c2162abae1afdb6446
Elías Heresi <elias.heresi@gmail.com>
2022-05-15 23:15:03 +0200

    Deleted more unused vars

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import { BrowserRouter } from "react-router-dom";
-import styled from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
