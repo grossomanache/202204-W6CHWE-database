dbc8b0c5b129f378732e109d3ae4aadd615d3bb2
Elías Heresi <elias.heresi@gmail.com>
2022-05-17 20:06:51 +0200

    Set token to local storage

--- src/redux/thunks/thunks.js
+++ src/redux/thunks/thunks.js
@@ -1,9 +1,11 @@
-const { loadRobotsActionCreator } = require("../features/robots/robotsSlice");
+import axios from "axios";
+import { loadRobotsActionCreator } from "../features/robots/robotsSlice";
 
 const token = `Bearer ${process.env.REACT_APP_TOKEN}`;
 
 export const loadRobotsThunk = () => async (dispatch) => {
-  const response = await fetch(process.env.REACT_APP_API_URL, {
+  const route = `${process.env.REACT_APP_API_URL}/robots`;
+  const response = await fetch(route, {
     method: "GET",
     headers: { Authorization: token },
   });
@@ -12,11 +14,17 @@ export const loadRobotsThunk = () => async (dispatch) => {
 };
 
 export const loadRobotByIdThunk = (id) => async (dispatch) => {
-  const route = `${process.env.REACT_APP_API_URL}/${id}`;
+  const route = `${process.env.REACT_APP_API_URL}/robots/${id}`;
   const response = await fetch(route, {
     method: "GET",
     headers: { Authorization: token },
   });
   const apiResponse = await response.json();
   dispatch(loadRobotsActionCreator([apiResponse]));
+};
+
+export const validateUserThunk = (loginInformation) => async (dispatch) => {
+  const route = `${process.env.REACT_APP_API_URL}/users/login`;
+  const { data } = await axios.post(route, loginInformation);
+  localStorage.setItem("token", data.token);
 };
