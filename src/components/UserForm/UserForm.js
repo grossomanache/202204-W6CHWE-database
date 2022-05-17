afb107eea254ec66ab238079524b45d91b6786df
Elías Heresi <elias.heresi@gmail.com>
2022-05-17 21:27:01 +0200

    Created form component and passed a useState hook

---
+++ src/components/UserForm/UserForm.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const UserForm = () => {
+  const initialFields = { username: "", password: "" };
+  const [formInformation, setFormInformation] = useState(initialFields);
+  const changeData = (event) => {
+    setFormInformation({
+      ...formInformation,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  return (
+    <form autoComplete="off" noValidate>
+      <label>
+        Name:{" "}
+        <input
+          id="username"
+          type="text"
+          value={formInformation.username}
+          onChange={changeData}
+        />
+      </label>
+      <label>
+        Password:{" "}
+        <input
+          id="password"
+          type="password"
+          value={formInformation.password}
+          onChange={changeData}
+        />
+      </label>
+    </form>
+  );
+};
+
+export default UserForm;
